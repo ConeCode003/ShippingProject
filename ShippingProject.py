a15b7da5975da8ad98851526e288dc6c2219e64c
ConeCode003 <154759836+ConeCode003@users.noreply.github.com>
2025-03-04 20:06:50 +0100

    Update ShippingProject.py

--- ShippingProject.py
+++ ShippingProject.py
@@ -1,9 +1,9 @@
 # Setting the weight of the package
 weight = 4.8
 
-#Ground Shipping
+#Ground Shipping is dependent on the weight of the package and flat charge
 if weight <= 2:
-  cost_ground = weight * 1.5 + 20
+  cost_ground = weight * 1.5 + 20 # weight*num1 + num2, (weight*num1) - price per pound and num2 - flat charge
 elif weight <=6:
   cost_ground = weight * 3.0 + 20
 elif weight <=10:
@@ -11,15 +11,17 @@ elif weight <=10:
 else:
   cost_ground = weight * 4.75 + 20
 
-print("Ground Shipping $", cost_ground)
+print("Ground Shipping $",cost_ground)
 
-#Ground Shipping Premium
+#Ground Shipping Premium is flat charge
 cost_ground_premium = 125.00
+
 print("Ground Shipping premium $",cost_ground_premium)
 
-#Drone Shipping
+#Drone Shipping is dependent on the weight of the package and flat charge,
+
 if weight <= 2:
-  cost_ground = weight * 4.50
+  cost_ground = weight * 4.50 # weight*num1 + num2,  where (weight*num1) is price per pound and num2 is flat charge
 elif weight <=6:
   cost_ground = weight * 9.00
 elif weight <=10:
